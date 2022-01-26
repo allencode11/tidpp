9bf00538820608a2ca5768488298a6ac7ad763a3
Alina Enache <alinaenache@Alinas-MacBook-Air.local>
2022-01-27 01:15:17 +0200

    delete submodule

---
+++ ui/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+import HomePage from './pages/Home.page'
+import ProductPage from './pages/Product.page'
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Router>
+          <Route path={'/'} component={HomePage} exact />
+            <Route path={'/product/:id'} component={ProductPage}/>
+        </Router>
+      </div>
+    </>
+  );
+}
+
+export default App;
